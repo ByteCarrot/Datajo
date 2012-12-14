341f30dac81605d0375b2ad2e0fa439fed7beeef
ByteCarrot <support@bytecarrot.com>
2012-12-14 18:32:19 +0100

    Added first Jasmine tests

---
+++ src/Datajo/EventName.spec.ts
@@ -0,0 +1,13 @@
+/// <reference path="Infrastructure/jasmine.d.ts" />
+/// <reference path="EventName.ts" />
+
+describe("EventName", () => {
+
+    describe("extract", () => {
+
+        it("should return false for everything what is not a JavaScript array", () => {
+        });
+
+    });
+
+});
--- src/Datajo/Underscore.spec.ts
+++ src/Datajo/Underscore.spec.ts
@@ -5,8 +5,55 @@ describe("Underscore", () => {
 
     describe("isArray", () => {
 
-        it("should return false for everything except a JavaScript array", () => {
+        it("should return false for everything what is not a JavaScript array", () => {
+            expect(_.isArray(undefined)).toBeFalsy();
+            expect(_.isArray(null)).toBeFalsy();
+            expect(_.isArray(0)).toBeFalsy();
+            expect(_.isArray('some string')).toBeFalsy();
+            expect(_.isArray({})).toBeFalsy();
+        });
 
+        it("should return true for the array", () => {
+            expect(_.isArray([])).toBeTruthy();
+            expect(_.isArray(new Array())).toBeTruthy();
+        });
+
+    });
+
+    describe("isString", () => {
+
+        it("should return false for everything what is not a string", () => {
+            expect(_.isString(undefined)).toBeFalsy();
+            expect(_.isString(null)).toBeFalsy();
+            expect(_.isString(0)).toBeFalsy();
+            expect(_.isString({})).toBeFalsy();
+            expect(_.isString([])).toBeFalsy();
+        });
+
+        it("should return true for a string", () => {
+            expect(_.isString('')).toBeTruthy();
+            expect(_.isString('some string')).toBeTruthy();
+            expect(_.isString(new String('some string'))).toBeTruthy();
+        });
+
+    });
+
+    describe("normalize", () => {
+
+        it("should remove trailing white spaces from the string", () => {
+            expect(_.normalize(' some string     ')).toEqual('some string');
+            expect(_.normalize('  ')).toEqual('');
+            expect(_.normalize('some string')).toEqual('some string');
+        });
+
+        it("should make all characters lower case", () => {
+            expect(_.normalize('Some String')).toEqual('some string');
+            expect(_.normalize('     Some String   ')).toEqual('some string');
+        });
+
+        it("should throw exception if the string has not been provided", () => {
+            expect(() => _.normalize(undefined)).toThrow();
+            expect(() => _.normalize(null)).toThrow();
         });
 
     });
