4bda40fb966812d13215242dab7d4e8d106943bf
ByteCarrot <support@bytecarrot.com>
2012-12-19 00:38:18 +0100

    Improved debug view

--- src/Datajo/ErrorHandler.ts
+++ src/Datajo/ErrorHandler.ts
@@ -6,6 +6,7 @@ class ErrorHandler {
     private debugMode: bool;
     constructor() {
         this.debugMode = QueryString.find('datajo') === 'debug';
+        $(window).resize(() => this.setViewerSize());
     }
     private prepareMessage(event: any, xhr: any, options: any, error: any): string {
         return '\n\n--- Datajo Ajax Error --- \n',
@@ -19,11 +20,12 @@ class ErrorHandler {
         var htmlContent = _.isString(contentType) && contentType.toLowerCase().indexOf('text/html') >= 0;
 
         var body = $('body');
+        
         body.prepend(
-            '<div id="datajo-debug" style="font-size:.9em;padding:0px;z-index:10000;overflow:auto;position:absolute;top:0px;left:0px;width:100%;height:100%;background-color:#f00;color:#fff">' +
-                '<table style="margin:0px;padding:0px;width:100%;height:100%;border:0px;">' + 
+            '<div id="datajo-debug" style="font-size:.9em;padding:0px;z-index:10000;position:absolute;top:0px;left:0px;width:100%;background-color:#f00;color:#fff">' +
+                '<table style="margin:0px;padding:0px;width:100%;border:0px;">' + 
                     '<tr>' + 
-                        '<td style="height:1%; padding:10px 20px 20px 20px;">' + 
+                        '<td style="height:1px; padding:10px 20px 0px 20px;">' + 
                             '<button style="float:right;font-weight:bold;color:#fff;background-color:#f00;border:0px;">x</button>' +
                             '<h3 style="margin:0px;padding:0px;">Datajo Ajax Error</h3>' + 
                             'Status: ' + xhr.status + '<br />' +
@@ -35,20 +37,35 @@ class ErrorHandler {
                         '</td>' + 
                     '</tr>' + 
                     '<tr>' + 
-                        '<td>' + 
-                            '<iframe style="width:100%;height:100%;border:0px;"></iframe>' + 
+                        '<td style="padding: 20px;">' + 
+                            '<iframe style="width:100%;border:solid 1px #f00;"></iframe>' + 
                         '</td>' + 
                     '</tr>' + 
                 '</table>' +
             '</div>');
+        
         var viewer = $('#datajo-debug');
         var button = viewer.find('button').click(() => {
             viewer.replaceWith('');
         });
-        var iframe = viewer.find('iframe').get(0).contentDocument;
-        iframe.open();
-        iframe.write(xhr.responseText);
-        iframe.close();
+        var iframe = viewer.find('iframe')
+        var doc = iframe.get(0).contentDocument;
+        doc.open();
+        doc.write(xhr.responseText);
+        doc.close();
+
+        this.setViewerSize();
+    }
+    private setViewerSize() {
+        var viewer = $('#datajo-debug');
+        if (viewer === undefined) {
+            return;
+        }
+
+        var wh = $(window).outerHeight();
+        viewer.height(wh);
+        viewer.find('table').height(wh);
+        viewer.find('iframe').height(wh - viewer.find('td:first').height() - 60);
     }
     public onAjaxError(event: any, xhr: any, options: any, error: any) : any {
         var message = this.prepareMessage(event, xhr, options, error);
--- src/Datajo/Library/datajo.js
+++ src/Datajo/Library/datajo.js
@@ -27,7 +27,11 @@ var _ = (function () {
 })();
 var ErrorHandler = (function () {
     function ErrorHandler() {
+        var _this = this;
         this.debugMode = QueryString.find('datajo') === 'debug';
+        $(window).resize(function () {
+            return _this.setViewerSize();
+        });
     }
     ErrorHandler.prototype.prepareMessage = function (event, xhr, options, error) {
         return '\n\n--- Datajo Ajax Error --- \n' , 'Status:      ' + xhr.status + '\n' + 'Status Text: ' + xhr.statusText + '\n' + 'Response Text:\n' + xhr.responseText + '\n' + '------------------------';
@@ -36,15 +40,27 @@ var ErrorHandler = (function () {
         var contentType = xhr.getResponseHeader('Content-Type');
         var htmlContent = _.isString(contentType) && contentType.toLowerCase().indexOf('text/html') >= 0;
         var body = $('body');
-        body.prepend('<div id="datajo-debug" style="font-size:.9em;padding:0px;z-index:10000;overflow:auto;position:absolute;top:0px;left:0px;width:100%;height:100%;background-color:#f00;color:#fff">' + '<table style="margin:0px;padding:0px;width:100%;height:100%;border:0px;">' + '<tr>' + '<td style="height:1%; padding:10px 20px 20px 20px;">' + '<button style="float:right;font-weight:bold;color:#fff;background-color:#f00;border:0px;">x</button>' + '<h3 style="margin:0px;padding:0px;">Datajo Ajax Error</h3>' + 'Status: ' + xhr.status + '<br />' + 'Status Text: ' + xhr.statusText + '<br>' + 'Headers: ' + '<div style="margin-left:30px;">' + (xhr.getAllResponseHeaders()).replace(/\n/g, '<br />') + '</div>' + '</td>' + '</tr>' + '<tr>' + '<td>' + '<iframe style="width:100%;height:100%;border:0px;"></iframe>' + '</td>' + '</tr>' + '</table>' + '</div>');
+        body.prepend('<div id="datajo-debug" style="font-size:.9em;padding:0px;z-index:10000;position:absolute;top:0px;left:0px;width:100%;background-color:#f00;color:#fff">' + '<table style="margin:0px;padding:0px;width:100%;border:0px;">' + '<tr>' + '<td style="height:1px; padding:10px 20px 0px 20px;">' + '<button style="float:right;font-weight:bold;color:#fff;background-color:#f00;border:0px;">x</button>' + '<h3 style="margin:0px;padding:0px;">Datajo Ajax Error</h3>' + 'Status: ' + xhr.status + '<br />' + 'Status Text: ' + xhr.statusText + '<br>' + 'Headers: ' + '<div style="margin-left:30px;">' + (xhr.getAllResponseHeaders()).replace(/\n/g, '<br />') + '</div>' + '</td>' + '</tr>' + '<tr>' + '<td style="padding: 20px;">' + '<iframe style="width:100%;border:solid 1px #f00;"></iframe>' + '</td>' + '</tr>' + '</table>' + '</div>');
         var viewer = $('#datajo-debug');
         var button = viewer.find('button').click(function () {
             viewer.replaceWith('');
         });
-        var iframe = viewer.find('iframe').get(0).contentDocument;
-        iframe.open();
-        iframe.write(xhr.responseText);
-        iframe.close();
+        var iframe = viewer.find('iframe');
+        var doc = iframe.get(0).contentDocument;
+        doc.open();
+        doc.write(xhr.responseText);
+        doc.close();
+        this.setViewerSize();
+    };
+    ErrorHandler.prototype.setViewerSize = function () {
+        var viewer = $('#datajo-debug');
+        if(viewer === undefined) {
+            return;
+        }
+        var wh = $(window).outerHeight();
+        viewer.height(wh);
+        viewer.find('table').height(wh);
+        viewer.find('iframe').height(wh - viewer.find('td:first').height() - 60);
     };
     ErrorHandler.prototype.onAjaxError = function (event, xhr, options, error) {
         var message = this.prepareMessage(event, xhr, options, error);
